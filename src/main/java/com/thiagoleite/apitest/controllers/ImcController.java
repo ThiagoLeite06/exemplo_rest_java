6be292e79ebd6b621dfddaa9638e3622c8c8300b
Thiago Leite <thiago.0686@gmail.com>
2023-06-29 11:21:49 -0300

    Stash to create service

--- src/main/java/com/thiagoleite/apitest/controllers/ImcController.java
+++ src/main/java/com/thiagoleite/apitest/controllers/ImcController.java
@@ -2,6 +2,10 @@ package com.thiagoleite.apitest.controllers;
 
 import com.thiagoleite.apitest.models.ImcRequest;
 import com.thiagoleite.apitest.models.ImcResponse;
+
+import com.thiagoleite.apitest.services.ImcService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,8 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ImcController {
+
     @PostMapping("/calculate")
     public ResponseEntity<ImcResponse> calculateIMC(@RequestBody ImcRequest imcRequest) {
+
+
+
+    @Autowired
+    private ImcService imcService;
+    @PostMapping("/calculate")
+    public ResponseEntity<ImcResponse> calculateIMC(@RequestBody ImcRequest imcRequest) {
+        imcService.CalculateImc(imcRequest.getHeight(), imcRequest.getWeight());
 
     }
 }
